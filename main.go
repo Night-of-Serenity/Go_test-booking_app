964e252056591ce40cafa2045a41dd639cf14b4c
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-23 21:38:13 +0700

    test array and slice

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
 
+	// var booking = []string{}
+	// booking := []string{}
+	var bookings []string
+
 	fmt.Printf("Welcome to %v booking application\n",conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n",conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
@@ -33,9 +37,17 @@ func main() {
 
 
 	remainingTickets -= userTickets
+	bookings  = append(bookings, firstName + " " + lastName)
+
+	// fmt.Printf("The whole slice: %v\n",bookings)
+	// fmt.Printf("The first value: %v\n",bookings[0])
+	// fmt.Printf("Slice type: %T\n",bookings)
+	// fmt.Printf("Slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v  for booking %v tickets. You will receive a confirmation email at %v \n",firstName,lastName,userTickets,email)
 	
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets,conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n",bookings)
 
 }	
